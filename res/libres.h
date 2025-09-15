a6c3443944444aee48f1644d8c72e45d25ba66f0
Andrea Baldiraghi <andrea.baldiraghi@gmail.com>
2025-09-16 07:50:27 +0800

    libres
    added libres

---
+++ res/libres.h
@@ -0,0 +1,9 @@
+#include <genesis.h>
+#ifndef _RES_LIBRES_H_
+#define _RES_LIBRES_H_
+
+extern const u8 stop_xgm[172];
+extern const Bitmap sgdk_logo;
+extern const TileSet font_default;
+
+#endif // _RES_LIBRES_H_
