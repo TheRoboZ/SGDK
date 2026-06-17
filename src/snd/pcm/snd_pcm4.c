9c5223b9d466ab2ac3b4d59ed1f74183735cdb4d
Stephane Dallongeville <stephane.dallongeville@pasteur.fr>
2026-06-17 22:37:16 +0200

    fixed prototypes

--- src/snd/pcm/snd_pcm4.c
+++ src/snd/pcm/snd_pcm4.c
@@ -88,7 +88,7 @@ bool NO_INLINE SND_PCM4_isPlaying(const u16 channel_mask)
     return ret;
 }
 
-void NO_INLINE SND_PCM4_startPlay(const u8 *sample, const u32 len, const SoundPCMChannel channel, const u8 loop)
+NO_INLINE void SND_PCM4_startPlay(const u8 *sample, const u32 len, const SoundPCMChannel channel, const bool loop)
 {
     vu8 *pb;
     u8 status;
